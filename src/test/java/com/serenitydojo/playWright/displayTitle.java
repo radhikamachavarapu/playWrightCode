38b21fb5b57d6b8424211dd8b5f5121758e09fad
Radhika <machavarapu.radhika@gmail.com>
2026-03-24 10:03:03 -0700

    Adding playwright project

---
+++ src/test/java/com/serenitydojo/playWright/displayTitle.java
@@ -0,0 +1,23 @@
+package com.serenitydojo.playWright;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class displayTitle {
+    @Test
+    void shouldShowThePageTitle() {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch();
+        Page page = browser.newPage();
+        page.navigate("https://practicesoftwaretesting.com/");
+        String title = page.title();
+        System.out.println("Title is: " + title);
+        Assertions.assertTrue(title.contains("Practice Software Testing"));
+        page.close();
+        browser.close();
+        playwright.close();
+    }
+}
