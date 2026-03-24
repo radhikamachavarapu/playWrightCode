87e846af7b28775ca6b1db3e129bd712ee209f55
Radhika <machavarapu.radhika@gmail.com>
2026-03-24 11:06:42 -0700

    new changes

--- src/test/java/com/serenitydojo/playWright/displayTitle_refactor.java
+++ src/test/java/com/serenitydojo/playWright/displayTitle_refactor.java
@@ -3,39 +3,51 @@ package com.serenitydojo.playWright;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class displayTitle {
-    @Test
-    void shouldShowThePageTitle() {
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        Page page = browser.newPage();
-        page.navigate("https://practicesoftwaretesting.com/");
-        String title = page.title();
-        System.out.println("Title is: " + title);
-        Assertions.assertTrue(title.contains("Practice Software Testing"));
-        String url = page.url();
-        System.out.println("URL is: " + url);
+public class displayTitle_refactor {
+
+    Playwright playwright;
+    Browser browser;
+    Page page;
+
+    @BeforeEach
+    void setup() {
+         playwright = Playwright.create();
+         browser = playwright.chromium().launch();
+         page = browser.newPage();
+    }
+
+    @AfterEach
+    void teardown() {
         page.close();
         browser.close();
         playwright.close();
     }
 
     @Test
+    void shouldShowThePageTitle() {
+
+        page.navigate("https://practicesoftwaretesting.com/");
+        String title = page.title();
+        System.out.println("Title is: " + title);
+        Assertions.assertTrue(title.contains("Practice Software Testing"));
+        String url = page.url();
+        System.out.println("URL is: " + url);
+
+    }
+
+    @Test
     void seachbyKeyword() {
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        Page page = browser.newPage();
         page.navigate("https://practicesoftwaretesting.com/");
         page.locator("[placeholder=Search]").fill("pliers");
         page.locator("button:has-text('Search')").click();
         int matchingResults = page.locator(".card").count();
         System.out.println("Number of matching results: " + matchingResults);
         Assertions.assertTrue(matchingResults > 0, "Expected to find at least one search result");
-        page.close();
-        browser.close();
-        playwright.close();
+
     }
 }
--- src/test/java/com/serenitydojo/playWright/displayTitle_usingPlayWrightAnnotation.java
+++ src/test/java/com/serenitydojo/playWright/displayTitle_usingPlayWrightAnnotation.java
@@ -3,39 +3,33 @@ package com.serenitydojo.playWright;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.junit.UsePlaywright;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-public class displayTitle {
+@UsePlaywright
+public class displayTitle_usingPlayWrightAnnotation {
+    
     @Test
-    void shouldShowThePageTitle() {
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        Page page = browser.newPage();
+    void shouldShowThePageTitle(Page page) {
+
         page.navigate("https://practicesoftwaretesting.com/");
         String title = page.title();
         System.out.println("Title is: " + title);
         Assertions.assertTrue(title.contains("Practice Software Testing"));
         String url = page.url();
         System.out.println("URL is: " + url);
-        page.close();
-        browser.close();
-        playwright.close();
-    }
 
+    }
     @Test
-    void seachbyKeyword() {
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        Page page = browser.newPage();
+    void seachbyKeyword(Page page) {
         page.navigate("https://practicesoftwaretesting.com/");
         page.locator("[placeholder=Search]").fill("pliers");
         page.locator("button:has-text('Search')").click();
         int matchingResults = page.locator(".card").count();
         System.out.println("Number of matching results: " + matchingResults);
         Assertions.assertTrue(matchingResults > 0, "Expected to find at least one search result");
-        page.close();
-        browser.close();
-        playwright.close();
+
     }
 }
